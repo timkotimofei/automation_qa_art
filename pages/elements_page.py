bde9727f1b9cfe89cc66ec107e606c614ac480bd
timkotimofei <timkotimofeiusa@gmail.com>
2024-11-02 11:34:30 -0500

    TestWebTable

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -2,6 +2,10 @@ import time
 import random
 from itertools import count
 
+from requests import delete
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.devtools.v127.storage import delete_storage_bucket
+
 from generator.generator import generated_person
 from locators.elements_page_locators import TextBookPageLocators, CheckBoxPageLocators, RadioButtonPageLocators, \
     WebTablePageLocators
@@ -103,7 +107,63 @@ class WebTablePage(BasePage):
             self.element_is_visible(self.locators.SUBMIT).click()
             time.sleep(1)
             count -= 1
-            return first_name, last_name, email, age, salary, department
+            return [first_name, last_name, str(age), email, str(salary), department]
+
+
+    def check_new_added_person(self):
+        people_list = self.elements_are_present(self.locators.FULL_PEOPLE_LIST)
+        data = []
+        for i in people_list:
+            data.append(i.text.splitlines())
+        return data
+
+    def search_some_person(self, key_word):
+        self.element_is_visible(self.locators.SEARCH_INPUT).send_keys(key_word)
+
+    def check_search_person(self):
+        # Моя задача здесь найти строку, но их всего 10 в таблице отображается и есть пустые строки
+        # Как найти непустые строки? Они содержат кнопку delete и значит по кнопке delete и находим
+        # ищем ее а потом переходим к родительскому элементу т е  к строке
+        delete_button = self.element_is_present(self.locators.DELETE_BUTTON)
+        row = delete_button.find_element("xpath",self.locators.ROW_PARENT)
+        return row.text.splitlines()
+
+
+    def update_person_info(self):
+        person_info = next(generated_person())
+        age = person_info.age
+        self.remove_footer()
+        self.element_is_visible(self.locators.UPDATE_BUTTON).click()
+        self.element_is_visible(self.locators.AGE_INPUT).clear()
+        self.element_is_visible(self.locators.AGE_INPUT).send_keys(age)
+        self.element_is_visible(self.locators.SUBMIT).click()
+        return str(age)
+
+    def delete_person(self):
+        self.remove_footer()
+        self.element_is_visible(self.locators.DELETE_BUTTON).click()
+
+    def check_deleted(self):
+        return self.element_is_present(self.locators.NO_ROWS_FOUND).text
+
+    def select_up_to_some_rows(self):
+        count = [5, 10, 20, 25, 50, 100]
+        data = []
+        for x in count:
+            count_row_button = self.element_is_visible(self.locators.COUNT_ROW_LIST)
+            self.go_to_element(count_row_button)
+            count_row_button.click()
+            self.element_is_visible((By.CSS_SELECTOR,f"option[value='{x}']")).click()
+            data.append(self.check_count_rows())
+        return data
+
+    def check_count_rows(self):
+        list_rows = self.elements_are_present(self.locators.FULL_PEOPLE_LIST)
+        return len(list_rows)
+
+
+
+
 
 
 
